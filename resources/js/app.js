1bc4912df7861d55655311f3019ae85ab054ed35
ludoguenet <ludovicguenet@gmx.com>
2024-05-11 14:06:05 +0200

    fix: added vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import IncrementCounter from './components/Counter.vue';
+
+createApp({})
+    .component('IncrementCounter', IncrementCounter)
+    .mount('#app')
